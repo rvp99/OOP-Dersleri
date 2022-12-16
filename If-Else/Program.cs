2f54014ab035bdafd48aa37508ea86a42a5f7289
rvp99 <serkan.alsabeyit@gmail.com>
2022-12-16 16:24:28 +0300

    16 Aralık 2022 Ders Notları

--- If-Else/Program.cs
+++ If-Else/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,70 @@ namespace If_Else
     {
         static void Main(string[] args)
         {
+
+            #region Soru - 1
+            /*
+            Verilen sayı eğer 0 ve 100 aralığındaysa ekrana “Sayı 0 ile 100 aralığındadır.”, eğer 100 ve 200 aralığındaysa ekrana “Sayı 100 ile 200 aralığındadır.”, eğer sayı 0’dan küçük veya 200’den büyükse de “Sayı 0’dan küçük veya 200’den büyüktür.” yazdırın.
+            */
+
+            #endregion
+
+            #region Ornek-1
+
+            //var number = -20;
+
+            //if (number >= 0 && number <= 100)
+            //{
+            //    Console.WriteLine("Sayı 0 ile 100 arasındadır.");
+            //}
+            //else if (number >= 100 && number <= 200)
+            //{
+            //    Console.WriteLine("Sayı 100 ile 200 arasındadır.");
+            //}
+            //else if (number < 0 || number > 200)
+            //{
+            //    Console.WriteLine("Sayı 0 dan küçük yada 200 den büyüktür.");
+            //}
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region Soru - 2
+
+            //Klavyeden girilen mevsimde bulunan ayları ekrana yazdırınız.
+
+            #endregion
+
+            #region Ornek - 2
+
+            //string mevsim;
+            //Console.Write("Bir mevsim adı giriniz : ");
+            //mevsim = Console.ReadLine();
+
+            //if (mevsim == "ilkbahar")
+            //    Console.Write(mevsim + " Mevsimindeki aylar Mart Nisan Mayıs");
+            //else if (mevsim == "yaz")
+            //    Console.Write(mevsim + " Mevsimindeki aylar Haziran Temmuz Ağustos");
+            //else if (mevsim == "sonbahar")
+            //    Console.Write(mevsim + " Mevsimindeki aylar Eylül Ekim Kasım");
+            //else if (mevsim == "kış")
+            //    Console.Write(mevsim + " Mevsimindeki aylar Aralık Ocak Şubat");
+            //else
+            //    Console.Write("Hatalı giriş yaptınız");
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region Odev
+
+            //Kullanıcıdan 3 adet sınav notu girmesini isteyin.Girilen sınav notlarının ortalaması 0 ve 50 aralığındaysa ekrana “Kaldınız”, 50 ve 100 aralığındaysa ekrana “Geçtiniz” yazdırın.Eğer bu değerler arasında değilse ekrana “Geçersiz Değer” yazdırın.
+
+            #endregion
+
+
+
         }
     }
 }
---
+++ VeriTipler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriTipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // En sık kullanılan değişken tipleri aşağıdaki giibidir.
+            // int = Tam sayı tutan değişken tipi.
+            // decimal = Ondalıklı sayı tutan değişken tipi.
+            // double = Ondalıklı sayı tutan değişken tipi.
+            // string = Metinsel değer tutan değişken tipi.
+            // char = Sadece tek harf tutabilen değişken tipi.
+            // bool = True yada False değer tutan değişken tipi.
+            // Bir değişken tanımlarken ilk olarak değişkenin tipini sonrada değişken ismini veririz. Aşağıda değişken tanımlama örneklerini görebilirsiniz.
+
+            int x = 12;
+            decimal y = 14.7M;          // Ondalıklı sayıları decimal tanımlarsak sonuna 'M' getiriyoruz.
+            double z = 123.5;
+            string name = "Serkan";     // Metin tipi değişkenler çift tırnak içine yazılırlar.
+            char a = 'a';
+            bool sonuc = true;
+        }
+    }
+}
